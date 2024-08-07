e7ee93dcfc04f082420bd19325bb439087b00748
dwigthDev <147568951+dwigthDev@users.noreply.github.com>
2024-08-07 11:10:22 -0400

    Update listaDeEstudiantes.py

--- listaDeEstudiantes.py
+++ listaDeEstudiantes.py
@@ -2,8 +2,6 @@
 #un nombre, una edad y una calificación. El programa debe permitir agregar nuevos estudiantes,
 #actualizar la calificación de un estudiante, listar todos los estudiantes y guardar/cargar la lista de
 #estudiantes desde un archivo en formato JSON
-
-import json
 import time 
 lista_curso = [
     {
@@ -14,7 +12,7 @@ lista_curso = [
 ]
 
 def barra_de_carga(): #Este bloque de codigo es una barra de carga
-    print("\nCargando... pi pu pi pu")
+    print("\nCargando...")
     tiempo_inicio = time.time()
     tiempo_actual = time.time()
     while tiempo_actual - tiempo_inicio < 2:
@@ -28,13 +26,8 @@ def barra_de_carga(): #Este bloque de codigo es una barra de carga
     # Imprimir la barra de carga completa al 100%
     print('\r[' + '=' * 100 + '] 100%')
 
-
-
-def menu ():
-        print("1.Agregar nuevo estudiante\n2.Actualizar_calificacion\n3.ver estudiantes\n4.guardar estudiantes\n5.cargar estudiantes\n6.salir")
-        
 #OPCION 1
-def agregar_estudiante(lista_curso, nombre, edad, calificacion):
+def agregar_estudiante(lista_curso, nombre, edad, calificacion): #se crea un diccionaro que representa a un estudiante y se guarda en la lsita de estudiantes
     estudiante = {
         "nombre": nombre,
         "edad": edad,
@@ -42,13 +35,13 @@ def agregar_estudiante(lista_curso, nombre, edad, calificacion):
     }
     lista_curso.append(estudiante)
     barra_de_carga()
-    print("\n Alumno agregado correctamente :) \n")
+    print("\n Alumno agregado correctamente :) \n") #mensaje de confirmacion 
     time.sleep(1)
     
 #OPCION 2
 def actualizar_calificacion(lista_curso, nombre, nota):
-    for alumno in lista_curso:
-            if alumno["nombre"] == nombre: #compara el nombre para ver si es la correcta
+    for alumno in lista_curso: #compara el nombre del alumno y al encontrarlo le modifica la nota
+            if alumno["nombre"] == nombre:
                 barra_de_carga()
                 alumno["calificacion"] = nota
                 print("***Editado Correctamente*** \n")
@@ -57,10 +50,10 @@ def actualizar_calificacion(lista_curso, nombre, nota):
 #OPCION 3
 def listar_estudiantes(lista_curso):
     barra_de_carga()
-    if len(lista_curso) == 0:
+    if len(lista_curso) == 0: # verifica que si no hay tareas creadas
         print("\nAun no hay tareas creadas\n")
     else:
-        print(f"\nEn total tienes {len(lista_curso)} alumnos:\n")
+        print(f"\nEn total tienes {len(lista_curso)} alumnos:\n") #por medio del FOR imprime en plantalla cada uno de los estudiantes 
         for alumno in lista_curso:
             print(f"Nombre del alumno: {alumno['nombre']}\n Edad: {alumno['edad']}\n calificacion: {alumno['calificacion']} \n")
             time.sleep(1)
@@ -68,7 +61,7 @@ def listar_estudiantes(lista_curso):
 #OPCION 4
 def guardar_estudiantes(lista_curso):
     barra_de_carga()
-    with open ("DB_Lista_curso.txt",'w') as database:
+    with open ("DB_Lista_curso.txt",'w') as database: #crea un archivo TXT y escribe sobre el los datos de los estudiantes
         lista = str(lista_curso)
         database.write(lista)
         print("\n***Guardado correctamente***\n")
@@ -78,25 +71,30 @@ def cargar_estudiantes():
     try:
         barra_de_carga()
         print("\n")
-        with open('DB_Lista_curso.txt', 'r') as database:
+        with open('DB_Lista_curso.txt', 'r') as database: #Toma el archivo anteriormente creado y lo imprime en pantalla
             database = database.read()
             print(database)    
         print("\n")
-    except FileNotFoundError:
+    except FileNotFoundError: #si no encuentra el archivo saldra error
         print("\nArchivo no encontrado... primero debe crear el archivo (opcion 4)\n")
         time.sleep(1)
 
-def validarOpcion(opcion):
+def menu ():
+        print("1.Agregar nuevo estudiante\n2.Actualizar_calificacion\n3.ver estudiantes\n4.guardar estudiantes\n5.cargar estudiantes\n6.salir")
+
+while True:
+    menu()
+    opcion = int(input("Ingresa la opcion que desees: "))
     if opcion == 1:
         try:
-            nombre = str(input("ingresa el nombre del alumno: "))
+            nombre = str(input("ingresa el nombre del alumno: ")).strip()
             edad = int(input("ingresa la edad del alumno: "))
             calificacion = float(input("ingresa la calificacion del alumno: "))
             agregar_estudiante(lista_curso, nombre, edad, calificacion)
         except ValueError:
             print("ingrese el tipo de dato adecuado")
     elif opcion == 2:
-        nombre = str(input("ingrea el nombre del alumno que quieres editar: "))
+        nombre = str(input("ingrea el nombre del alumno que quieres editar: ")).strip()
         nota = float(input("ingresa la nueva nota del alumno: "))
         actualizar_calificacion(lista_curso, nombre, nota)
     elif opcion == 3:
@@ -104,9 +102,9 @@ def validarOpcion(opcion):
     elif opcion == 4:
         guardar_estudiantes(lista_curso)
     elif opcion == 5:
-        cargar_estudiantes()   
-           
-while True:
-    menu()
-    opcion = int(input("Ingresa la opcion que desees: "))
-    validarOpcion(opcion)
+        cargar_estudiantes() 
+    elif opcion == 6:
+        print("saliendo del programa...")
+        time.sleep(1)
+        print("fin del programa")
+        break
