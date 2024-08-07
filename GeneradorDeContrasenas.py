96708d8b952edb2645dee745719fe677d734790a
dwigthDev <147568951+dwigthDev@users.noreply.github.com>
2024-08-07 11:31:22 -0400

    Update GeneradorDeContrasenas.py

--- GeneradorDeContrasenas.py
+++ GeneradorDeContrasenas.py
@@ -1,10 +1,13 @@
-import string
+#importacion de la librerias necesarias
+import string 
 import random
+
 def generar(longitud):
     caracteres = string.ascii_letters + string.digits + string.punctuation
     contraseña = "".join(random.choice(caracteres) for i in range (longitud))
     print("Tu contraseña es:", contraseña)
 print("Bienvenidos al generador de contraseñas... \npara continuar ingrese la longitud de tu contraseña")
+
 while True:
     try:
         longitud = int(input("---> "))
